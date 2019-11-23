de34c6b424727c4e5bdcd25dcbff65aa2a27f956
ROMAVICTO <davis.con.arturoroma2002@gmail.com>
2019-11-22 21:42:57 -0600

    mi primer commit

---
+++ programacion.js
@@ -0,0 +1,150 @@
+var clic = 1;
+
+function divLogin1(){ 
+
+   if(clic==1){
+
+   document.getElementById("caja").style.height = "100%";
+   document.getElementById("boton1").style.display = 'inline';
+   document.getElementById("imagen_1").style.display='inline';
+
+   //clic = clic + 1;
+
+   //} else{
+
+   //    document.getElementById("caja").style.height = "0px";      
+   //    document.getElementById("boton1").style.display = 'none';
+   // clic = 1;
+
+   }   
+
+}
+
+ function divLogin1_img(){
+    if (clic1==1) {
+        document.getElementById("caja").style.height = '0px';
+        document.getElementById("boton1").style.display = 'none';
+        document.getElementById("imagen_1").style.display='none';
+    };
+ }
+
+
+var clic1=1;
+
+function divLogin2()
+{
+  if(clic1==1)
+  {
+    document.getElementById("caja").style.height="100%";
+    document.getElementById("boton2").style.display = 'inline';
+    document.getElementById("imagen_2").style.display='inline';
+  }
+}
+
+function divLogin2_img(){
+  if(clic1==1){
+    document.getElementById("caja").style.height = '0px';
+    document.getElementById("boton2").style.display = 'none';
+    document.getElementById("imagen_2").style.display='none';
+  }
+}
+
+var clic2=1;
+
+function divLogin3()
+{
+  if(clic2==1)
+  {
+    document.getElementById("caja").style.height="100%";
+    document.getElementById("boton3").style.display = 'inline';
+    document.getElementById("imagen_3").style.display='inline';
+  }
+}
+
+function divLogin3_img(){
+  if(clic2==1){
+    document.getElementById("caja").style.height = '0px';
+    document.getElementById("boton3").style.display = 'none';
+    document.getElementById("imagen_3").style.display='none';
+  }
+}
+
+var clic3=1;
+
+function divLogin4()
+{
+  if(clic3==1)
+  {
+    document.getElementById("caja").style.height="100%";
+    document.getElementById("boton4").style.display = 'inline';
+    document.getElementById("imagen_4").style.display='inline';
+  }
+}
+
+function divLogin4_img(){
+  if(clic3==1){
+    document.getElementById("caja").style.height = '0px';
+    document.getElementById("boton4").style.display = 'none';
+    document.getElementById("imagen_4").style.display='none';
+  }
+}
+
+var clic4=1;
+
+function divLogin5()
+{
+  if(clic4==1)
+  {
+    document.getElementById("caja").style.height="100%";
+    document.getElementById("boton5").style.display = 'inline';
+    document.getElementById("imagen_5").style.display='inline';
+  }
+}
+
+function divLogin5_img(){
+  if(clic4==1){
+    document.getElementById("caja").style.height = '0px';
+    document.getElementById("boton5").style.display = 'none';
+    document.getElementById("imagen_5").style.display='none';
+  }
+}
+
+var clic5=1;
+
+function divLogin6()
+{
+  if(clic5==1)
+  {
+    document.getElementById("caja").style.height="100%";
+    document.getElementById("boton6").style.display = 'inline';
+    document.getElementById("imagen_6").style.display='inline';
+  }
+}
+
+function divLogin6_img(){
+  if(clic5==1){
+    document.getElementById("caja").style.height = '0px';
+    document.getElementById("boton6").style.display = 'none';
+    document.getElementById("imagen_6").style.display='none';
+  }
+}
+
+var clic6=1
+
+function divLogin7()
+{
+  if(clic6==1)
+  {
+    document.getElementById("caja").style.height="100%";
+    document.getElementById("boton7").style.display = 'inline';
+    document.getElementById("imagen_7").style.display='inline';
+  }
+}
+
+function divLogin7_img(){
+  if(clic6==1){
+    document.getElementById("caja").style.height = '0px';
+    document.getElementById("boton7").style.display = 'none';
+    document.getElementById("imagen_7").style.display='none';
+  }
+}
